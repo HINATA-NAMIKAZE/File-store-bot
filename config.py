83b6f9a5cccb1e5dfafaaa6fd854f2dc55fcbd14
MAHESH KADALI <101107025+MAHESH-KADALI@users.noreply.github.com>
2024-05-23 17:55:09 +0530

    Add files via upload

--- config.py
+++ config.py
@@ -22,10 +22,10 @@ API_HASH = os.environ.get("API_HASH", "be67fa66bf79d732d799ed8fc7d54a16")
 CHANNEL_ID = int(os.environ.get("CHANNEL_ID", " -1002102934554"))
 
 # NAMA OWNER
-OWNER = os.environ.get("OWNER", "Kakashi_13")
+OWNER = os.environ.get("OWNER", "maheshsirop")
 
 #OWNER ID
-OWNER_ID = int(os.environ.get("OWNER_ID", "2052951004"))
+OWNER_ID = int(os.environ.get("OWNER_ID", "1596559467"))
 
 #Port
 PORT = os.environ.get("PORT", "8030")
@@ -41,16 +41,16 @@ FORCE_SUB_CHANNEL2 = int(os.environ.get("FORCE_SUB_CHANNEL2", ""))
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 #start message
-START_MSG = os.environ.get("START_MESSAGE", "<b>ʙᴀᴋᴋᴀᴀᴀ!! {first}\n\n ɪ ᴀᴍ ғɪʟᴇ sᴛᴏʀᴇ ʙᴏᴛ, ɪ ᴄᴀɴ sᴛᴏʀᴇ ᴘʀɪᴠᴀᴛᴇ ғɪʟᴇs ɪɴ sᴘᴇᴄɪғɪᴇᴅ ᴄʜᴀɴɴᴇʟ ᴀɴᴅ ᴏᴛʜᴇʀ ᴜsᴇʀs ᴄᴀɴ ᴀᴄᴄᴇss ɪᴛ ғʀᴏᴍ sᴘᴇᴄɪᴀʟ ʟɪɴᴋ.</b>")
+START_MSG = os.environ.get("START_MESSAGE", "<b>ʙᴀᴋᴋᴀᴀᴀ!! {first}\n\n I am a permanent file Sharing bot and users can access stored messages by using a shareable link given by me.</b>")
 try:
-    ADMINS=[6376328008]
-    for x in (os.environ.get("ADMINS", "2052951004 1596559467").split()):
+    ADMINS=[2052951004]
+    for x in (os.environ.get("ADMINS", "2052951004").split()):
         ADMINS.append(int(x))
 except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
 
 #Force sub message 
-FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "<b>ʜᴇʟʟᴏ {first}\nᴊᴏɪɴ ᴏᴜʀ ᴄʜᴀɴɴᴇʟs ᴀɴᴅ ᴛʜᴇɴ ᴄʟɪᴄᴋ ᴏɴ ʀᴇʟᴏᴀᴅ button ᴛᴏ ɢᴇᴛ ʏᴏᴜʀ ʀᴇǫᴜᴇꜱᴛᴇᴅ ꜰɪʟᴇ.</b>")
+FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "<b>Please Join My Channel to use this Bot!\n\n Due to Overload, Only Channel Subscribers can use this Bot!</b>")
 
 #set your Custom Caption here, Keep None for Disable Custom Caption
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", "<b>• ʙʏ @Hanime_Boys</b>")
@@ -62,10 +62,10 @@ PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" e
 DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True'
 
 BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
-USER_REPLY_TEXT = "ʙᴀᴋᴋᴀ ! ʏᴏᴜ ᴀʀᴇ ɴᴏᴛ ᴍʏ ꜱᴇɴᴘᴀɪ!!"
+USER_REPLY_TEXT = "Bakka ! You Are Not My Master!!"
 
 ADMINS.append(OWNER_ID)
-ADMINS.append(1596559467)
+ADMINS.append(2052951004)
 
 LOG_FILE_NAME = "mahesh-file-store-bot.txt"
 
