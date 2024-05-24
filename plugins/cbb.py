1b2cf88a2ae1743f7bf3ea11b189118381e38cb5
MAHESH KADALI <101107025+MAHESH-KADALI@users.noreply.github.com>
2024-05-24 11:04:07 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ ᴏᴡɴᴇʀ : Mahesh</a>\n○Support : <a href='https://t.me/+ecyVPv5vlPUwOTg1'>Hentai Support</a>\n○ Developer: <a>Mahesh</a>\n○ ᴏᴜʀ ᴄᴏᴍᴍᴜɴɪᴛʏ : <a href='https://t.me/Hanime_Boys'>Hentai</a></b>",
+            text = f"<b>○ ᴏᴡɴᴇʀ : Kakashi</a>\n○Support : <a href='https://t.me/+ecyVPv5vlPUwOTg1'>Hentai Support</a>\n○ Developer: <a>Mahesh</a>\n○ ᴏᴜʀ ᴄᴏᴍᴍᴜɴɪᴛʏ : <a href='https://t.me/Hanime_Boys'>Hentai</a></b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
